a1137f5cc2914a45d0387fdd2324e395c228390f
vuhongdang18092004 <hongdang18092004@gmail.com>
2026-06-15 22:54:16 +0700

    chore: initial spring boot setup

---
+++ code/backend/src/test/java/com/ioc/internship/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioc.internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
